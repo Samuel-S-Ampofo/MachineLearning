1b532d3e3f36aa9306ebce38442f1f415208bb46
Samuel Ampofo <samuel.ampofo@neosinnovation.com>
2020-01-19 14:41:09 +0100

    pythro intro donen

--- life.py
+++ life.py
@@ -2,6 +2,10 @@ import math
 
 theAnswer = 42
 
+# objects
+# mehtods are functions that belong to an object
+
+
 def qoute_marvin():
     print('I\'ve calculated your chances of survival but I don\'t you\'ll like that')
 
