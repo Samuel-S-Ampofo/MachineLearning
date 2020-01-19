f673b9fa5538155f841c49d598f516560a69c97f
Samuel Ampofo <samuel.ampofo@neosinnovation.com>
2020-01-19 12:12:14 +0100

    some progessn on sunday

---
+++ life.py
@@ -0,0 +1,10 @@
+import math
+
+theAnswer = 42
+
+def qoute_marvin():
+    print('I\'ve calculated your chances of survival but I don\'t you\'ll like that')
+
+def square_root(value):
+    return  math.sqrt(value)
+    
